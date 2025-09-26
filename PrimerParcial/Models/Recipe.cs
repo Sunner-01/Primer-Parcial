9e6d9bbc41840a1537fbc6db7e402a5d7a7880d9
Sunner-01 <sunerbarrera228@gmail.com>
2025-09-25 21:24:49 -0400

    Creacion de recipe

---
+++ PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
+{
+    public class Recipe
+    {
+        // Clave Primaria (PK)
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; } 
+
+        [Required]
+        public string Instructions { get; set; } 
+
+        public int PreparationTimeMinutes { get; set; }
+
+        public int Servings { get; set; } 
+
+        public DateTime DateCreated { get; set; } 
+
+        // --- Relaciones de Entity Framework Core ---
+
+        // Clave Foránea (FK): Vincula esta receta a una categoría
+        public int CategoryId { get; set; }
+
+        // Propiedad de Navegación: El lado 'uno' para la Categoría
+        public Category Category { get; set; }
+
+        // Propiedad de Navegación: El lado 'muchos' para los Ingredientes
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
